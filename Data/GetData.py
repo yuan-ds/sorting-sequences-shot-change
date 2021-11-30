f7f063a28d17802579fa1afa2694414e81dbfbaa
Ray Liu <36522461+tliu292@users.noreply.github.com>
2021-11-30 00:01:47 -0500

    Files to generate data

---
+++ Data/GetData.py
@@ -0,0 +1,103 @@
+import os,inspect
+import pandas as pd
+from tqdm import tqdm
+import pdb
+from GetShotLists import GetShotLists
+import random
+
+"""
+120 classes, each list corresponds to a class
+eg. Label 0 - [0, 1, 2, 3, 4]
+
+[[0, 1, 2, 3, 4], [0, 1, 2, 4, 3], [0, 1, 3, 2, 4], [0, 1, 3, 4, 2], 
+ [0, 1, 4, 2, 3], [0, 1, 4, 3, 2], [0, 2, 1, 3, 4], [0, 2, 1, 4, 3], 
+ [0, 2, 3, 1, 4], [0, 2, 3, 4, 1], [0, 2, 4, 1, 3], [0, 2, 4, 3, 1], 
+ [0, 3, 1, 2, 4], [0, 3, 1, 4, 2], [0, 3, 2, 1, 4], [0, 3, 2, 4, 1], 
+ [0, 3, 4, 1, 2], [0, 3, 4, 2, 1], [0, 4, 1, 2, 3], [0, 4, 1, 3, 2], 
+ [0, 4, 2, 1, 3], [0, 4, 2, 3, 1], [0, 4, 3, 1, 2], [0, 4, 3, 2, 1], 
+ [1, 0, 2, 3, 4], [1, 0, 2, 4, 3], [1, 0, 3, 2, 4], [1, 0, 3, 4, 2], 
+ [1, 0, 4, 2, 3], [1, 0, 4, 3, 2], [1, 2, 0, 3, 4], [1, 2, 0, 4, 3], 
+ [1, 2, 3, 0, 4], [1, 2, 3, 4, 0], [1, 2, 4, 0, 3], [1, 2, 4, 3, 0], 
+ [1, 3, 0, 2, 4], [1, 3, 0, 4, 2], [1, 3, 2, 0, 4], [1, 3, 2, 4, 0], 
+ [1, 3, 4, 0, 2], [1, 3, 4, 2, 0], [1, 4, 0, 2, 3], [1, 4, 0, 3, 2], 
+ [1, 4, 2, 0, 3], [1, 4, 2, 3, 0], [1, 4, 3, 0, 2], [1, 4, 3, 2, 0], 
+ [2, 0, 1, 3, 4], [2, 0, 1, 4, 3], [2, 0, 3, 1, 4], [2, 0, 3, 4, 1], 
+ [2, 0, 4, 1, 3], [2, 0, 4, 3, 1], [2, 1, 0, 3, 4], [2, 1, 0, 4, 3], 
+ [2, 1, 3, 0, 4], [2, 1, 3, 4, 0], [2, 1, 4, 0, 3], [2, 1, 4, 3, 0], 
+ [2, 3, 0, 1, 4], [2, 3, 0, 4, 1], [2, 3, 1, 0, 4], [2, 3, 1, 4, 0], 
+ [2, 3, 4, 0, 1], [2, 3, 4, 1, 0], [2, 4, 0, 1, 3], [2, 4, 0, 3, 1], 
+ [2, 4, 1, 0, 3], [2, 4, 1, 3, 0], [2, 4, 3, 0, 1], [2, 4, 3, 1, 0], 
+ [3, 0, 1, 2, 4], [3, 0, 1, 4, 2], [3, 0, 2, 1, 4], [3, 0, 2, 4, 1], 
+ [3, 0, 4, 1, 2], [3, 0, 4, 2, 1], [3, 1, 0, 2, 4], [3, 1, 0, 4, 2], 
+ [3, 1, 2, 0, 4], [3, 1, 2, 4, 0], [3, 1, 4, 0, 2], [3, 1, 4, 2, 0], 
+ [3, 2, 0, 1, 4], [3, 2, 0, 4, 1], [3, 2, 1, 0, 4], [3, 2, 1, 4, 0], 
+ [3, 2, 4, 0, 1], [3, 2, 4, 1, 0], [3, 4, 0, 1, 2], [3, 4, 0, 2, 1], 
+ [3, 4, 1, 0, 2], [3, 4, 1, 2, 0], [3, 4, 2, 0, 1], [3, 4, 2, 1, 0], 
+ [4, 0, 1, 2, 3], [4, 0, 1, 3, 2], [4, 0, 2, 1, 3], [4, 0, 2, 3, 1], 
+ [4, 0, 3, 1, 2], [4, 0, 3, 2, 1], [4, 1, 0, 2, 3], [4, 1, 0, 3, 2], 
+ [4, 1, 2, 0, 3], [4, 1, 2, 3, 0], [4, 1, 3, 0, 2], [4, 1, 3, 2, 0], 
+ [4, 2, 0, 1, 3], [4, 2, 0, 3, 1], [4, 2, 1, 0, 3], [4, 2, 1, 3, 0], 
+ [4, 2, 3, 0, 1], [4, 2, 3, 1, 0], [4, 3, 0, 1, 2], [4, 3, 0, 2, 1], 
+ [4, 3, 1, 0, 2], [4, 3, 1, 2, 0], [4, 3, 2, 0, 1], [4, 3, 2, 1, 0]]
+"""
+
+numFrames = 5 # number of frames from the same shot
+nextShotSize = 80 
+
+currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+shot_list_path = currentdir + '/ShotList.csv'
+
+gsl = GetShotLists(shot_list_path)
+shotList = gsl.getshotlist()
+
+cleanedShotList = []
+for shot in shotList:
+    newShot = []
+    for frame in shot:
+        frame = frame.replace("[", "").replace("]", "")
+        frame = frame.strip().replace("'", "")
+        newShot.append(frame)
+    cleanedShotList.append(newShot)
+
+i = 0
+res = []
+while i < len(cleanedShotList)-nextShotSize:
+    new_shot = []
+    curr_shot = cleanedShotList[i]
+    shotSize = len(curr_shot)
+
+    if shotSize < numFrames:
+        i += 1
+        continue
+    
+    # add one frame from next few shots
+    for j in range(1, nextShotSize):
+        new_shot = []
+        gap = int(shotSize / numFrames)
+        for k in range(0, shotSize-gap+1, gap):
+            idx = random.randint(k, k+gap-1)
+            new_shot.append(curr_shot[idx])
+        if len(new_shot) != numFrames:
+            new_shot = new_shot[:numFrames]
+
+        s = cleanedShotList[i+j][0]
+        res.append(new_shot + [s])
+    i += 1
+
+final_res, orders = [], []
+for shot in res:
+    x = list(enumerate(shot))
+    random.shuffle(x)
+    indices, l = zip(*x)
+    final_res.append(l)
+    orders.append(list(indices))
+
+new_orders = []
+unique_orders = sorted([list(x) for x in set(tuple(x) for x in orders)])
+for e in orders:
+    new_orders.append(unique_orders.index(e))
+
+df = pd.DataFrame({"frames": final_res, "label": new_orders})
+df.to_csv("out.csv")
+
+
---
+++ Data/GetShotLists.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov  8 04:30:09 2021
+
+@author: TianrunWang
+"""
+import os,inspect
+import pandas as pd
+
+class GetShotLists:
+    
+    def __init__(self,path):
+        self.SL = pd.read_csv(path)
+        
+    def getshotlist(self):
+        SL = self.SL
+        shot_list = []
+        for i in range(len(SL)):
+            shots = SL['FrameLists'][i].split(',')#[1:-2]
+            shot_list.append(shots)    
+        return shot_list
+        
